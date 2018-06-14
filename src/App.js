3178f43647767a2f9e2d3e7796d0409d274a6329
Miguel Cobas <cobas.miguel@gmail.com>
2018-06-13 19:17:04 -0500

    Login Google
    Add Auth with Google.
    Add state to validate if user is different to null and add logout button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
+
 import './App.css';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      user: null,
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount (){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+   handleAuth (){
+     const provider = new firebase.auth.GoogleAuthProvider();
+    
+     firebase.auth().signInWithPopup(provider)
+     .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+     .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+    
+    }
+    handleLogout (){
+      const provider = new firebase.auth.GoogleAuthProvider();
+     
+      firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+     
+   
+     
+     }
+  
+    renderLoginButton () {
+      //Si el usuario esta logueado
+      if (this.state.user) {
+        return(
+          <div>
+          <img className="App-logo" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <button onClick={this.handleLogout}>Salir</button>
+            <p>
+            Hola  {this.state.user.displayName}!!
+            </p>
+          </div>
+        )
+       
+      }else{
+        return(
+          <button onClick={this.handleAuth}>Login con Google</button>
+        )
+      }
+
+      //Si no esta logueado
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Psudogram </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.renderLoginButton() }
         </p>
       </div>
     );
